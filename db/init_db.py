7c85cd56ad499ea3e4cd0a4119b52c572d4fe78f
brunoconterato <brunoconterato@gmail.com>
2025-06-04 18:21:13 -0300

    fix: reorder database initialization to drop tables before recreation

--- db/init_db.py
+++ db/init_db.py
@@ -2,12 +2,11 @@ from database_session import get_db
 from models import *
 from database_session import Base
 
-
 # Database initialization
 def init_db():
     with get_db() as db:
-        Base.metadata.create_all(db.bind)
-
+        Base.metadata.drop_all(db.bind)    # Drop all tables
+        Base.metadata.create_all(db.bind)  # Recreate all tables
 
 if __name__ == "__main__":
     init_db()
