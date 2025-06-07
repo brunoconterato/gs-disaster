667989c21b80330b4f01698e72ac586203d4048d
Amandha Nery <neryamandha@gmail.com>
2025-06-06 21:49:49 -0300

    ajust db/database_session.py

--- db/database_session.py
+++ db/database_session.py
@@ -13,7 +13,7 @@ load_dotenv()
 # Database connection configuration
 DB_CONFIG = {
     'host': os.getenv('POSTGRES_HOST'),
-    'port': os.getenv('POSTGRES_PORT'),
+    'port': int(os.getenv('POSTGRES_PORT')),
     'database': os.getenv('POSTGRES_DB'),
     'user': os.getenv('POSTGRES_USER'),
     'password': os.getenv('POSTGRES_PASSWORD')
